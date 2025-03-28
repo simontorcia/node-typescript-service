b12e44faf6279ad50e958a2e779a81b5d553bf05
simone-mxm <simone.fermani@musixmatch.com>
2025-03-29 00:07:41 +0100

    add validation on email and pwd

--- src/validations/userValidation.ts
+++ src/validations/userValidation.ts
@@ -5,11 +5,15 @@ export const userSchema = Joi.object({
   surname: Joi.string().trim().min(2).max(50).required(),
   birth_date: Joi.date().iso().required(),
   sex: Joi.string().valid('male', 'female', 'other').required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).required(),
 });
 
 export const updateUserSchema = Joi.object({
-  name: Joi.string().min(2).max(50),
-  surname: Joi.string().min(2).max(50),
+  name: Joi.string().trim().min(2).max(50),
+  surname: Joi.string().trim().min(2).max(50),
   birth_date: Joi.date().iso(),
   sex: Joi.string().valid('male', 'female', 'other'),
+  email: Joi.string().email(),
+  password: Joi.string().min(8),
 });
