26ddad8d55439260c6c681f1fc6376fc5ca6c05e
simone-mxm <simone.fermani@musixmatch.com>
2025-03-29 01:26:35 +0100

    add validation middleware

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -4,22 +4,23 @@ import { validateBody, validateParams, validateQuery } from '../middlewares/vali
 import { userSchema, updateUserSchema } from '../validations/userValidation';
 import { idParamSchema } from '../validations/paramsValidation';
 import { paginationQuerySchema } from '../validations/queryValidation';
+import { authenticateToken } from '../middlewares/authenticateToken';
 
 const router = Router();
 
 // Create a new user
-router.post('/', validateBody(userSchema), UserController.createUser);
+router.post('/', authenticateToken, validateBody(userSchema), UserController.createUser);
 
 // Get a paginated list of users
-router.get('/', validateQuery(paginationQuerySchema), UserController.getUsers);
+router.get('/', authenticateToken, validateQuery(paginationQuerySchema), UserController.getUsers);
 
 // Get a single user by ID
-router.get('/:id', validateParams(idParamSchema), UserController.getUserById);
+router.get('/:id', authenticateToken, validateParams(idParamSchema), UserController.getUserById);
 
 // Update a user by ID
-router.put('/:id', validateParams(idParamSchema), validateBody(updateUserSchema), UserController.updateUser);
+router.put('/:id', authenticateToken, validateParams(idParamSchema), validateBody(updateUserSchema), UserController.updateUser);
 
 // Delete a user by ID
-router.delete('/:id', validateParams(idParamSchema), UserController.deleteUser);
+router.delete('/:id', authenticateToken, validateParams(idParamSchema), UserController.deleteUser);
 
 export default router;
